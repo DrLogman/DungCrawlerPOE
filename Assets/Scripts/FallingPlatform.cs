632f330509b7240ad11672001ff83c92ea02149f
JoblessDrummer <jcwespinosa@gmail.com>
2023-04-28 13:46:45 -0400

    aa

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    //he fell off
+    [SerializeField] float fallingDelay;
+    [SerializeField] float destroyDelay;
+    [SerializeField] Rigidbody2D rb2d;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))  
+        {
+            StartCoroutine(Fall());
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+    private IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallingDelay);
+        rb2d.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(gameObject, destroyDelay);
+    }
+}
---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    
+    
+    //spiky
+    
+    void Start()
+    {
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("Spikes!!");
+            
+        }
+    }
+     
+}
